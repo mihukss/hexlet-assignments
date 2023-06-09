73332f675656a18afe90d955081cb69200e416e4
@hexlet/cli <support@hexlet.io>
2023-06-09 18:52:32 +0300

    submit rails-basics-ru/models

--- rails-basics-ru/models/config/routes.rb
+++ rails-basics-ru/models/config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'articles/index', as: 'articles_page' 
-  get "articles/show", as: 'show_articles'
-  root 'home#index'
   # BEGIN
-  
+  get 'articles/index', as: 'articles_index' 
+  get "articles/show", as: 'articles_show'
+  root 'home#index'  
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
